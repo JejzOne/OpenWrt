d2e23e767ca6c4af1aa2a7a7c3b2766c02b446ff
JejzOne <jejzbfsx@gmail.com>
2025-12-19 20:17:22 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -136,7 +136,7 @@ git_clone 22.03 https://github.com/sbwml/feeds_packages_net_aria2 aria2
 clone_dir main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
 
 # 看门狗(菜单项放到服务里面)
-clone_dir main https://github.com/sirpdboy/luci-app-watchdog.git luci-app-watchdog
+clone_dir main https://github.com/sirpdboy/luci-app-watchdog.git luci-app-watchdog watchdog
 sed -i '/"admin\/control"[[:space:]]*:/,/^[[:space:]]*},/d' $destination_dir/luci-app-watchdog/root/usr/share/luci/menu.d/luci-app-watchdog.json
 sed -i 's#"admin/control/#"admin/services/#g' $destination_dir/luci-app-watchdog/root/usr/share/luci/menu.d/luci-app-watchdog.json
 
