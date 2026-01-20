532130f62b545704ffd1faf3c903605d17d12f8a
JejzOne <jejzbfsx@gmail.com>
2026-01-20 13:05:43 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -224,6 +224,7 @@ clone_dir main https://github.com/sbwml/openwrt_pkgs luci-app-netspeedtest speed
 # 报错修复
 # rm -rf feeds/packages/utils/v2dat
 # cp -f $GITHUB_WORKSPACE/personal/xray-core/Makefile feeds/packages/net/xray-core/Makefile
+clone_dir https://github.com/sbwml/openwrt_helloworld xray-core
 
 # openclash
 clone_dir master https://github.com/vernesong/OpenClash luci-app-openclash
