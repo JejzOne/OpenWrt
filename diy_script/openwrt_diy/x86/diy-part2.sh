c978c4fd6d99dd9ee99416915223b3aebd77d4af
JejzOne <jejzbfsx@gmail.com>
2026-03-31 09:56:49 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -222,7 +222,6 @@ clone_dir main https://github.com/sbwml/openwrt_pkgs luci-app-netspeedtest speed
 # rm -rf feeds/packages/utils/v2dat
 # cp -f $GITHUB_WORKSPACE/personal/xray-core/Makefile feeds/packages/net/xray-core/Makefile
 clone_dir https://github.com/sbwml/openwrt_helloworld xray-core
-sed -i 's/+!LINUX_6_12:kmod-iptables/+(!LINUX_6_12\&\&!LINUX_6_18):kmod-iptables/' package/kernel/linux/modules/netfilter.mk
 
 # openclash
 clone_dir master https://github.com/vernesong/OpenClash luci-app-openclash
