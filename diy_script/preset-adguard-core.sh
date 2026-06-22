9e8c4e66c474875f8301894e082ea60aee30e172
JejzOne <jejzbfsx@gmail.com>
2026-06-22 10:33:16 +0800

    Update preset-adguard-core.sh

--- diy_script/preset-adguard-core.sh
+++ diy_script/preset-adguard-core.sh
@@ -2,21 +2,15 @@
 
 [ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
 
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep -oE "https://[^ ]*AdGuardHome_linux_${1}\.tar\.gz" | head -n1)
+LATEST_TAG=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep '"tag_name"' | awk -F '"' '{print $4}')
 
-if [[ -z "$AGH_CORE" || "$AGH_CORE" != *.tar.gz ]]; then
-  echo "⚠️ 未找到适用于 ${1} 的 AdGuardHome 下载链接！"
-  LATEST_TAG=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep '"tag_name"' | awk -F '"' '{print $4}')
-  if echo "$LATEST_TAG" | grep -Eq '^v[0-9]+(\.[0-9]+)*$'; then
-    echo "📌 最新版本号: $LATEST_TAG"
-  else
-    LATEST_TAG="v0.107.77"
-    echo "⚠️ 无法获取最新版本号，使用备用版本: ${LATEST_TAG}"
-  fi
+if echo "$LATEST_TAG" | grep -Eq '^v[0-9]+(\.[0-9]+)*$'; then
+  echo "📌 最新版本号: $LATEST_TAG"
+else
+  LATEST_TAG="v0.107.77"
+  echo "⚠️ 无法获取最新版本号，使用备用版本: ${LATEST_TAG}"
   AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/download/${LATEST_TAG}/AdGuardHome_linux_${1}.tar.gz"
   echo "✅ 已启用备用下载地址"
-else
-    echo "✅ 已获取最新适用于 ${1} 的 AdGuardHome 下载链接"
 fi
 
 echo "📥 下载地址: $AGH_CORE"
