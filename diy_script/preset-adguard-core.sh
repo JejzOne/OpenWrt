733b904dda67d934c6442795149f3466179ea936
JejzOne <jejzbfsx@gmail.com>
2026-06-22 10:37:13 +0800

    Update preset-adguard-core.sh

--- diy_script/preset-adguard-core.sh
+++ diy_script/preset-adguard-core.sh
@@ -9,9 +9,10 @@ if echo "$LATEST_TAG" | grep -Eq '^v[0-9]+(\.[0-9]+)*$'; then
 else
   LATEST_TAG="v0.107.77"
   echo "⚠️ 无法获取最新版本号，使用备用版本: ${LATEST_TAG}"
-  AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/download/${LATEST_TAG}/AdGuardHome_linux_${1}.tar.gz"
   echo "✅ 已启用备用下载地址"
 fi
+
+AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/download/${LATEST_TAG}/AdGuardHome_linux_${1}.tar.gz"
 
 echo "📥 下载地址: $AGH_CORE"
 
