2f8b314fe66d1a99667ac76da1e47359c813896b
JejzOne <jejzbfsx@gmail.com>
2026-08-29 12:26:45 +0800

    Update diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -214,7 +214,7 @@ if [ "$REPO_BRANCH" == "master" ]; then
     echo "✅"
 fi
 mkdir -p feeds/packages/utils/dockerd/patches
-wget -O ./feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch https://raw.githubusercontent.com/AndyChiang888/packages/refs/heads/dockerd/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
+wget -O ./feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch https://raw.githubusercontent.com/AndyChiang888/packages/fe37485e1fb0b4b09a954174509ce782459590a2/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
 
 # openclash
 clone_dir master https://github.com/vernesong/OpenClash luci-app-openclash
